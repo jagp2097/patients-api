bd24bcc0da92dadaad676e030f2b3d40bfb30559
jagp2097 <jagp2097@gmail.com>
2020-09-29 19:17:19 -0500

    Sequelize folder deleteed.

--- sequelize/app.js
+++
@@ -1,9 +0,0 @@
-const dbConfig = require('./db.config')
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize(dbConfig.dbName, dbConfig.user, dbConfig.password, {
-    dialect: dbConfig.dialect,
-    host: dbConfig.host
-})
-
-module.exports = sequelize
